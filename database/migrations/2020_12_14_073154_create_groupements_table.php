d759c1b07bfe228cdff00ed546192761f20ba35e
Jean Lionel <nijeanlionel@gmail.com>
2020-12-16 06:18:16 +0200

    add groupement

---
+++ database/migrations/2020_12_14_073154_create_groupements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groupements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('colline_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groupements');
+    }
+}
